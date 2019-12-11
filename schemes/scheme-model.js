320e9c6392c1699a3ed8562672d182c89b6140e9
adeoye adela <adeoye.adela@gmail.com>
2019-12-12 00:54:13 +0100

    findSteps  helper working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,17 +1,24 @@
-const db =require("../data/db-config")
+const db = require("../data/db-config");
 module.exports = {
-    find,
-    findById,
-    // findSteps,
-    // add,
-    // update,
-    // remove,
+  find,
+  findById,
+  findSteps
+  // add,
+  // update,
+  // remove,
+};
+function find() {
+  return db("schemes");
 }
-     function find(){
-        return db( "schemes")
-    }
 
-    function findById(id){
-        return db("schemes")
-        .where({id})
-    }
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+function findSteps(id) {
+  return db("steps as s")
+    .select("s.id","schemes.scheme_name","s.step_number","s.instructions")
+    .join("schemes", "s.scheme_id", "schemes.id")
+    .where("s.id", id);
+}
